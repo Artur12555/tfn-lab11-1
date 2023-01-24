3e302ac075ae2e656a93d8d764c785eb9d0addec
sugarvalley <cqrkowa@gmail.com>
2023-01-24 13:48:57 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+
+import Person from "./components/Person";
 
 function App() {
+  const [person, setPerson] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  async function fetchPerson() {
+    fetch("https://randomuser.me/api/")
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw response;
+        })
+        .then(person => setPerson(person))
+        .catch(error => {
+          console.log(error);
+          setError(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        })
+  }
+
+  useEffect(() => {
+    fetchPerson();
+  }, []);
+
+  if (loading) return <h1>loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Person person={person}/>
+        <button onClick={() => fetchPerson()}>Generate new person</button>
+      </>
   );
 }
 
